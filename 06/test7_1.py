7589e229ebc9717bc17c2ec32d03b9c0aceb94fa
GalynaPylypenko26 <pgv0pgv@gmail.com>
2024-11-16 21:20:45 +0300

    test7_1

---
+++ 06/test7_1.py
@@ -0,0 +1,7 @@
+def say_hi(name, age):
+    return f"Hi. My name is {name} and I'm {age} years old"
+# Перевірка, що функція повертає рядок
+Test1 = say_hi("Alex", 32)
+print(Test1)
+Test2 = say_hi("Frank", 68)
+print(Test2)
